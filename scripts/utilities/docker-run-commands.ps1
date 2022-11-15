7f1a2809cb6383443920e9d9d4a9e41eca2f00f2
Carter Blouin <blouinc@arizona.edu>
2022-11-15 14:15:08 -0700

    Updated path for my local laptop

--- scripts/utilities/docker-run-commands.ps1
+++ scripts/utilities/docker-run-commands.ps1
@@ -1,8 +1,5 @@
 # local r docker container running on port 8787
 # this command disables authentication so only use it on a localhost/private network
 docker run -d -e DISABLE_AUTH=true -e ROOT=TRUE --rm -p 127.0.0.1:8787:8787 `
--v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction `
--v C:/Users/andre/temp:/home/rstudio/temp `
--v G:/cameratraps:/home/rstudio/cameratraps `
--v G:/cameratraps2:/home/rstudio/cameratraps2 `
-amantaya/rocker-verse:4.0.5
+-v C:/Users/bloui/Heber_Veg_Report/grazing-interaction:/home/rstudio/grazing-interaction `
+>> amantaya/rocker-verse:4.0.5
